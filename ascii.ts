aaa774b200944137b32f3cc399596e9e2ea9bf05
Top <108082973+human-d3v@users.noreply.github.com>
2024-04-25 12:37:11 -0600

    Adding ascii

---
+++ ascii.ts
@@ -0,0 +1,50 @@
+// export const logo:string = 
+// `
+//                                 
+//               S?S               
+//             S*+;+?S             
+//      S?%%%%S?*****?%%????%      
+//      S???%S @@@@@@ S%?***%      
+//      %%% @@@@@@ @@@@@@ %?%      
+//   S%*?S @@@@@@   @@@@@@@S%?%SS  
+// S?**+*@@@@@@       @@@@@@%?***S 
+//  S?*+?@@@             @@@S?*?S  
+//    %*% @@@@@        @@@@@S%%    
+//    ?;+%@@@@@@@   @@@@@@@S?+?    
+//   %;;;;%@@@@@@@ @@@@@@ %++++%   
+//   S??*+*SS @@@@@@@@@S%?**??%S   
+//         S????%%S%??***%         
+//          %***?%S%?***%          
+//           %SS      SS           
+//                                 
+// `;
+
+import chalk from "chalk";
+
+export const logo:string = 
+`
+█████████████████████████████████████████████████
+███████████████████████${chalk.green('▓╬▓')}███████████████████████
+█████████████████████${chalk.yellow('╬╬')}${chalk.green('╬╬╬╬')}▓█████████████████████
+███████████████████${chalk.yellow('╬╠')}${chalk.green('╠╠╬╬╬╬╬╬')}${chalk.whiteBright('▓')}███████████████████
+████████${chalk.magentaBright('▓')}${chalk.magenta('╬╬╬╬╬╬')}${chalk.red('╬')}${chalk.red('╬╣')}${chalk.yellow('▓▓▓▓')}${chalk.green('╬╬╬╬╬╣')}${chalk.greenBright('▓▓▓')}${chalk.cyan('╬╬╬')}${chalk.blue('╬╬╬╠╠╠')}${chalk.blue('▓')}████████
+████████${chalk.magentaBright('▓╬')}${chalk.magenta('╬╬╬╬╬')}${chalk.red('▓')}█████████████${chalk.greenBright('▓▓')}${chalk.green('▓')}${chalk.cyan('╬╬╬')}${chalk.blue('╬╬╠╠╫▓')}████████
+████████${chalk.magentaBright('▓')}${chalk.magenta('╬╬╬╬╬')}${chalk.magentaBright('▓')}█████████${chalk.whiteBright('█')}█████████${chalk.cyan('▓╬╬')}${chalk.blue('╬╬╠╫')}████████
+████████${chalk.magentaBright('▓╣')}${chalk.magenta('╣▓╣')}██████████${chalk.whiteBright('███')}██████████${chalk.magenta('╬')}${chalk.cyan('▓')}${chalk.blue('╬╬╣')}████████
+██████${chalk.cyan('╬Ñ╣▓╣')}${chalk.magenta('▓')}██████████${chalk.whiteBright('█████')}██████████▓▓${chalk.magenta('▓')}${chalk.red('╬╬╬')}██████
+██${chalk.cyan('▓╬╬')}${chalk.cyanBright('╠╠▒')}${chalk.magenta('╟╣')}███████████${chalk.whiteBright('███████')}███████████${chalk.red('╣╬╬╬╬')}${chalk.yellow('╬╬▓')}██
+█${chalk.cyan('╬╬╬╬╠╠')}${chalk.blue('▒╟')}██████████${chalk.whiteBright('███████████')}██████████${chalk.red('╬╬╬')}${chalk.yellow('╬╬╬╬╬')}█
+██${chalk.cyan('▓╬╬╠')}${chalk.blue('╠▒')}${chalk.blue('▓')}████████${chalk.whiteBright('███████████████')}████████${chalk.red('▓╬╬╬')}${chalk.yellow('╬╬▓')}██
+████${chalk.cyanBright('▒')}${chalk.cyan('╠╠')}${chalk.blueBright('▓')}${chalk.blue('▓')}█████${chalk.whiteBright('█████████████████████')}█████${chalk.redBright('▓▓')}${chalk.yellowBright('╬')}${chalk.yellow('╬╣')}████
+██████${chalk.yellowBright('╣▓')}${chalk.green('▓')}████████${chalk.whiteBright('███████████████')}████████${chalk.magentaBright('▓▓')}${chalk.redBright('▓')}██████
+█████${chalk.redBright('Ñ')}${chalk.red('▒╬')}${chalk.yellowBright('▓▓')}█████████${chalk.whiteBright('███████████')}█████████${chalk.magentaBright('▓')}${chalk.yellowBright('▓')}${chalk.green('╬╬▓')}█████
+████${chalk.yellowBright('▓')}${chalk.redBright('▒▒')}${chalk.red('╬')}${chalk.yellow('▒╠')}███████████${chalk.whiteBright('███████')}██████████${chalk.yellowBright('▓')}${chalk.greenBright('▓')}${chalk.green('╬╬╬╬╬')}█████
+████${chalk.redBright('Ñ▒╬╬╬╬')}${chalk.yellow('╠▓')}██████████${chalk.whiteBright('█████')}█████████${chalk.greenBright('▓╬')}${chalk.green('╬╬╬╬╬╣')}████
+███${chalk.redBright('▓▒▒')}${chalk.redBright('▒╬╬╬╬')}${chalk.yellow('╠▓')}${chalk.yellowBright('▓')}█████████${chalk.whiteBright('███')}█████████${chalk.greenBright('▓▓')}${chalk.green('╬╬╬╬╬╬╬')}${chalk.yellow('╬▓')}███
+█████████${chalk.yellow('▓▓')}${chalk.magentaBright('▓▓▓▓')}${chalk.magenta('▓▓')}███████████████${chalk.greenBright('▓▓▓')}${chalk.yellow('▓▓▓▓')}██████████
+█████████████${chalk.redBright('╬')}${chalk.yellow('╬╬╬╬╬')}${chalk.redBright('╬╬')}${chalk.magenta('╣╬╬╬╬')}${chalk.blueBright('╬╬')}${chalk.blue('╬╠')}${chalk.cyan('╠╠╠')}${chalk.blueBright('╠╠╣')}█████████████
+██████████████${chalk.yellowBright('▒╬')}${chalk.yellow('╬╬╬╬╬')}${chalk.redBright('╬╬╬')}${chalk.cyan('▓')}${chalk.blueBright('╬╠╠')}${chalk.cyan('╠╠╠╠')}${chalk.blue('╠╠╟')}██████████████
+███████████████${chalk.yellowBright('▒╬')}${chalk.yellow('╬╬▒▓')}${chalk.yellowBright('▓')}█████${chalk.cyanBright('▓')}${chalk.cyan('▓')}${chalk.blueBright('▒╠')}${chalk.blue('╠╠╟')}███████████████
+████████████████${chalk.yellowBright('▓')}${chalk.yellow('╠')}██████████████${chalk.blue('╠')}${chalk.blueBright('▓')}███████████████
+█████████████████████████████████████████████████
+`
